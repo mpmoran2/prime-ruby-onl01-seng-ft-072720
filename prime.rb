77456fba92f40d063946626af7a1438464d6f768
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 21:52:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(value)
+  if value <= 1 || value == 0 || value == 1 #making sure it falls into what I need it to find. would work with just <=1 i think?
+    return false
+  elsif
+    (2..value - 1).each do |i| 
+      if value % i == 0 #can it be divided by itself and 1? If no, not a prime
+        return false
+      end
+    end
+  end
+  true  #true if it is a prime.
+end
+
