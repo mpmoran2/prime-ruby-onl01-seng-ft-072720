9ee58ded22175b792a6c87afd07d3049e3aebe5b
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 21:42:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,15 @@
-def prime?(num)
-  
+def prime?(x)
+  if x == 1
+    return true 
+  elsif x < 1
+    return false
+  else
+  for y in 2..(x-1)
+    if (x % y) == 0
+      return false 
+    end
+  end 
+end
+
+  true
 end
