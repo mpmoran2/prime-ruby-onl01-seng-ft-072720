06b508665a91ce7ea7cb0c0e142341b60df24f48
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 21:49:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(value)
-  if value <= 1 || value == 0 || value == 1 #what are primes. what values looing for
+  if value <= 1 || value == 0 || value == 1 #making sure it falls into what I need it to find. would work with just <=1 i think?
     return false
   elsif
     (2..value - 1).each do |i|
       if value % i == 0
-      return false
+        return false
+      end
     end
   end
-end
-true  
+  true  # <= need explanation
 end
 
